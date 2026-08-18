6352cbdb4760c0beb4581b85d00f2c357d6f0f40
HeetJain <73645495+HeetJain@users.noreply.github.com>
2026-08-18 20:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-import streamlit as st;
+import streamlit as st
+
+st.title("Ai-socratic-tutor")
+st.write("[bold]your own friendly neighbourhood Lupa ")
 st.write("Namaste Duniya")
+input = st.chat_input("Type Your message here")
+
+if "message" not in st.session_state:
+    user = st.chat_message("role",avatar="ai").write("Hi my name is Lupa. How may i help you?")
+    
+if input:
+    st.write(f"the user is saying {input}")
