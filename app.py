006cd37a6a7cae528a3938502bce01546e0fdd92
HeetJain <73645495+HeetJain@users.noreply.github.com>
2026-08-18 22:53:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 
 st.title("Ai-socratic-tutor")
-st.write("your own friendly neighbourhood Lupa ")
+st.write("[bold]your own friendly neighbourhood Lupa ")
 st.write("Namaste Duniya")
-input = st.chat_input("Type Your message here")
 
 if "message" not in st.session_state:
-    user = st.chat_message("role",avatar="ai").write("Hi my name is Lupa. How may i help you?")
-    
+    st.session_state.message = [{"role":"ai","content":"Hello my name is lupa"}]
+
+for msg in st.session_state.message:
+     with st.chat_message(msg["role"]):
+         st.write(msg["content"])
+
+
+input = st.chat_input("enter you question here")
+
 if input:
-    st.write(f"the user is saying {input}")
+    st.session_state.message.append({"role":"user","content":input})
+    with st.chat_message("user"):
+        st.write(input)
+
+
+
+    bot_reply=f"You asked: {input}, right now?.But, I am not connected to any api because my non coders friend haven't made one yet"
+    st.session_state.message.append({"role":"ai","content": bot_reply})
+    with st.chat_message("ai"):
+        st.write(bot_reply)
